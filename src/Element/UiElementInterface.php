4d67e19c88b51562fe37a47775f64db13781309f
Anton Ukhanev <xedin.unknown@gmail.com>
2020-04-18 17:17:49 +0200

    Add modifiers to `UiElementInterface`
    Also, `getType()` can now throw

--- src/Element/UiElementInterface.php
+++ src/Element/UiElementInterface.php
@@ -7,6 +7,7 @@ use Dhii\Content\Type\TypeInterface;
 use Dhii\Content\Type\UiTypeInterface;
 use Dhii\Util\String\DescriptionAwareInterface;
 use Dhii\Util\String\LabelAwareInterface;
+use Exception;
 
 /**
  * An element of the user interface.
@@ -20,6 +21,30 @@ interface UiElementInterface extends
      * Retrieves the element's type.
      *
      * @return UiTypeInterface The type.
+     *
+     * @throws Exception If problem retrieving.
      */
     public function getType(): TypeInterface;
+
+    /**
+     * Creates a new instance with the specified label.
+     *
+     * @param string $label The label for the new instance.
+     *
+     * @return static A new instance of this class.
+     *
+     * @throws Exception If problem creating.
+     */
+    public function withLabel(string $label);
+
+    /**
+     * Creates a new instance with the specified description.
+     *
+     * @param string $description The description for the new instance.
+     *
+     * @return static A new instance of this class.
+     *
+     * @throws Exception If problem creating.
+     */
+    public function withDescription(string $description);
 }
